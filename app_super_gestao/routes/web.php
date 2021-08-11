dba0560fae2ef6a261ea9ae13d15231232238ff4
alvaropotel <alvaropotel@gmail.com>
2021-08-11 22:03:04 +0100

    Created new routes and grouping them with prefix

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -23,4 +23,10 @@ Route::get('/about-us', "AboutUsController@aboutUs");
 
 Route::get('/contact', "ContactController@contact");
 
+Route::get('/login', function(){ return 'Login'; });
 
+Route::prefix('/app')->group(function(){
+    Route::get('/clients', function(){ return 'Clients'; });
+    Route::get('/providers', function(){ return 'Providers'; });
+    Route::get('/products', function(){ return 'Products'; });
+});
