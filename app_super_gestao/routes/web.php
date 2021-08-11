12ba9175f26a3c6ca3b78184af2cd769dd3a52ea
alvaropotel <alvaropotel@gmail.com>
2021-08-11 21:20:00 +0100

    Working with parameteres

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -23,3 +23,7 @@ Route::get('/about-us', "AboutUsController@aboutUs");
 
 Route::get('/contact', "ContactController@contact");
 
+Route::get('/contact/{name?}/{category?}/{address?}/{message?}/', 
+    function(string $name = 'Alvaro', string $category = 'Eletronic',string $address = 'Niteroi',string $message = 'Make Money'){
+        echo $name . ", you are in " . $address . " at " . $category . " recibe a message " . $message;
+});
