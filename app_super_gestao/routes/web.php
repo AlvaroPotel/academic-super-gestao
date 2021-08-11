e3d4e860050d4953ef4b5f86cf99979e1c91cfd7
alvaropotel <alvaropotel@gmail.com>
2021-08-11 20:47:35 +0100

    Created Controllers and route modification

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -13,15 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return "Hello Alvaro!";
-});
+/* Route::get('/', function () {
+    return "Hello New APP!";
+}); */
 
-Route::get('/about-us', function () {
-    return "About Us!";
-});
+Route::get('/', 'MainController@main');
 
-Route::get('/contact', function () {
-    return "Contact!";
-});
+Route::get('/about-us', "AboutUsController@aboutUs");
+
+Route::get('/contact', "ContactController@contact");
 
