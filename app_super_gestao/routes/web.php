206fcb18742b85978ec49bfcd548fca79d3be939
alvaropotel <alvaropotel@gmail.com>
2021-08-11 15:40:26 +0100

    Create About Us and Contact routes

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -14,5 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return "Hello Alvaro!";
 });
+
+Route::get('/about-us', function () {
+    return "About Us!";
+});
+
+Route::get('/contact', function () {
+    return "Contact!";
+});
+
